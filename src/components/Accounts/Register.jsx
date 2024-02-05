ca95b82da63b14a2a53e41690ebce389c47ddbf9
ngaleano13 <140446186+ngaleano13@users.noreply.github.com>
2024-02-05 14:57:05 -0300

    Update Register.jsx

--- src/components/Accounts/Register.jsx
+++ src/components/Accounts/Register.jsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import '../Accounts/accounts.css'
+import '../Accounts/Accounts.css'
 import { useTranslation } from 'react-i18next';
 import axios from 'axios';
 import { useNavigate } from 'react-router-dom';
